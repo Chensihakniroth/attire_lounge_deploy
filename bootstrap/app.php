eb9c3e3d7b0f1138615f6686e77c0251c2e028b7
Niroth <spam099099@gmail.com>
2026-01-11 18:57:12 +0700

    Refactor code structure for improved readability and maintainability

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies('*');
-        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
