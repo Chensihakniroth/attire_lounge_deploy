a9f2292765b897efd6abcc43a300c2dee221d298
Niroth <spam099099@gmail.com>
2026-01-11 18:16:59 +0700

    Revert "s"
    This reverts commit 2c833a456a966ac275c40503f1064a69b22569ed.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,10 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies('*');
-        $middleware->append(\App\Http\Middleware\Cors::class);
-        $middleware->validateCsrfTokens(except: [
-            'api/*'
-        ]);
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
