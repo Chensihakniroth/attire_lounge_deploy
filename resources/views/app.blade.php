8b7fac96afed8fb730f8f8d66a88c911d38b24c3
Niroth <spam099099@gmail.com>
2026-02-13 19:21:51 +0700

    Fix asset paths in app.blade.php to use leading slash for consistency

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,10 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=Bayon&family=Kantumruy+Pro:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}?v={{ time() }}" rel="stylesheet">
+        <link href="{{ asset('/css/app.css') }}?v={{ time() }}" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
-        <script type="module" src="{{ asset('js/app.js') }}?v={{ time() }}"></script>
+        <script type="module" src="{{ asset('/js/app.js') }}?v={{ time() }}"></script>
     </body>
 </html>
